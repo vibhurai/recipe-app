33d596f064b0c1c519204f36c1303df8095d2ee6
krai20 <kaustubh_rai@optum.com>
2023-02-21 19:23:03 +0530

    recipe component layout completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "Recipe desc", "https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-12-2048x1267.jpg"),
+    new Recipe("Recipe 2", "desc 2", "https://static.toiimg.com/thumb/53110049.cms?imgsize=148092&width=509&height=340")
+  ];
 
+  ngOnInit(){
+
+  }
+
+  constructor(){
+
+  }
 }
