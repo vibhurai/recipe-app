18dec8db9680b5ac894c063a43625f0c0a787345
krai20 <kaustubh_rai@optum.com>
2023-02-23 12:25:43 +0530

    shopping list created

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Butter', 10),
+    new Ingredient('Clove', 5),
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
